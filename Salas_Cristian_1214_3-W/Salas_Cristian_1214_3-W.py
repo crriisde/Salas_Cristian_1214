c65f86ab5f2d89f1b32f4ac1b677f10714f74811
crriisde <a.23308051281214@cbtis128.edu.mx>
2024-09-26 16:18:41 -0600

    Add files via upload

---
+++ Salas_Cristian_1214_3-W/Salas_Cristian_1214_3-W.py
@@ -0,0 +1,12 @@
+print ("Salas De La O Cristian David 3-W 1214")
+Apellido_P = (input("Ingresa tu nombre paterno: ")) #Esta linea se encarga de definir la variable de el apellido paterno 
+Apellido_M =(input("Ingresa tu nombre materno: ")) #Esta linea se encarga de definir la variable de el apellido materno
+Nombre_1 = (input("Ingresa tu primer nombre: ")) #Esta linea se encarga de definfir el primer nombre
+Nombre_2 = (input("Ingresa tu segundo nombre: ")) #Esta linea se encargar de definir el segundo nombre
+Num_Control = (input("Ingresa tu numero de control: ")) #Esta linea se encarga de definir el numero de matricula
+Grado_Grupo = (input("Ingresa tu grupo y grado: ")) #Esta linea se encarga de definir el grado y grupo
+print ("Tus datos: ")
+print (Apellido_P, Apellido_M, Nombre_1, Nombre_2, Num_Control, Grado_Grupo ) #Esta linea se encarga de mostrar el conjunto de todas las variables
+
+print ("Tus datos en mayusculas: ") #esta linea se encarga de mostrar el aviso de lo que se mostrar
+print (Apellido_P.upper(), Apellido_M.upper(), Nombre_1.upper(), Nombre_2.upper(), Num_Control.upper (), Grado_Grupo.upper ()) #esta linea se encarga de mostrar los datos mayusculas
